e43c3cf5a9efce6a9452cbc073b005ebee657ab8
MakuMutts <kazminaviktoriya@gmail.com>
2026-08-24 14:58:11 +0300

    File Reader with multithreading

--- final_2/main.cpp
+++ final_2/main.cpp
@@ -1,17 +1,40 @@
 #include <iostream>
+#include <thread>
 #include <fstream>
 #include <string>
 #include <format>
+#include <vector>
 
 using namespace std;
 
-int main(){
-    int n = 1;
-    string filename = "file" + to_string(n) + ".txt";
+void ReadFile(int fileNumber){
+    string filename = "file" + to_string(fileNumber) + ".txt";
+    cout << "File: " + filename + " readed" << endl;
     fstream file(filename); 
     string line;
     while (getline(file, line)){
         cout << line << endl;
     }
     file.close();
+}
+
+void StartWork(vector<thread>& vec, int count){
+    for(int i = 0; i < count; i++){
+        thread t(ReadFile, i);
+        vec.push_back(move(t));
+    }
+}
+
+void FinishWork(vector<thread>& vec){
+    for(auto& v : vec){
+        v.join();
+        cout << "Thread finished" << endl;
+    }
+}
+
+
+int main(){
+    vector<thread> threads;
+    StartWork(threads, 3);
+    FinishWork(threads);
 }
